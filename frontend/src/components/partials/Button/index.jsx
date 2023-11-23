01ab166da35171491dae65c955c61d9ec1b69a31
vtrsg <vittorioschmittg@gmail.com>
2023-11-23 20:04:57 -0300

    refact: adapting the button for more contexts

--- frontend/src/components/partials/Button/index.jsx
+++ frontend/src/components/partials/Button/index.jsx
@@ -3,17 +3,21 @@ import { Link, NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { ButtonArea } from './styled';
 
-const Button = ({ text, route }) => {
+const Button = ({ text, route, onClick, type }) => {
+    const buttonProps = route ? { as: NavLink, to: route } : { type, onClick };
+
     return (
-        <Link to={route} component={NavLink}>
-            <ButtonArea>{text}</ButtonArea>
-        </Link>
+        <ButtonArea as={route && NavLink} {...buttonProps}>
+            {text}
+        </ButtonArea>
     );
 };
 
 Button.propTypes = {
     text: PropTypes.string.isRequired,
-    route: PropTypes.string.isRequired,
+    route: PropTypes.string,
+    onClick: PropTypes.func,
+    type: PropTypes.string,
 };
 
 export default Button;
--- frontend/src/components/partials/Button/styled.jsx
+++ frontend/src/components/partials/Button/styled.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonArea = styled.button`
-    width: 200px;
+    width: 250px;
     height: 30px;
     border-radius: 10px;
     background: rgba(149, 116, 205, 1);
@@ -10,7 +10,12 @@ export const ButtonArea = styled.button`
     font-weight: 600;
     box-shadow: 0px 1px 4px 0px rgba(187, 195, 206, 0.6);
     cursor: pointer;
-    a {
-        text-decoration: none;
+    text-decoration: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    @media (max-width: 430px) {
+        width: 90px;
+        font-size: 12px;
     }
 `;
