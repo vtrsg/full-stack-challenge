34511aefcb09a7c1d30ff8668b8dffa4f77fea37
vtrsg <vittorioschmittg@gmail.com>
2023-11-23 22:49:39 -0300

    refact: page not found

--- frontend/src/pages/NotFound/index.jsx
+++ frontend/src/pages/NotFound/index.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { NotFoundArea } from './styled';
+import Button from '../../components/partials/Button';
 
 const NotFound = () => {
     return (
         <NotFoundArea>
             <h1>Page not found</h1>
-
-            <Link to="/">Go to Home!!</Link>
+            <Button text="Go Home!!" route="/" />
         </NotFoundArea>
     );
 };
